4e6a5c00da9ed3c9e8bcd033ad37053a8c2c272b
LafTools Team - FX <work7z@outlook.com>
2024-01-31 01:06:57 +0800

    Update Git attributes and metadata generation

--- modules/elb-web/app/page.tsx
+++ modules/elb-web/app/page.tsx
@@ -1,11 +1,25 @@
 import Image from "next/image";
+import type { Metadata, ResolvingMetadata } from "next";
+import Head from 'next/head'
+import { Props } from "next/script";
+
 
 export default function Home() {
   return (
     <main className="">
-      <h1 className="bg-sky-200">
-        are you ok
+      <h1 className="">
+        <span>English Learning Base III</span>  {Math.random()}
       </h1>
     </main>
   );
+}
+
+
+export async function generateMetadata(
+  { }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  return {
+    title: "English Learning Base III",
+  };
 }
