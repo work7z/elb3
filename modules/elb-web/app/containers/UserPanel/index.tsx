a5d9f1d848e50791b53139f6382ec25491584047
LafTools Team - FX <work7z@outlook.com>
2024-02-03 16:33:59 +0800

    Refactor UserPanel and page components

--- modules/elb-web/app/containers/UserPanel/index.tsx
+++ modules/elb-web/app/containers/UserPanel/index.tsx
@@ -1,6 +1,9 @@
 import { COMMON_CLZ_ANCHOR_TEXT } from "@/app/common/clz"
 import RegularLink from "@/app/components/RegularLink"
+import { MoonIcon, Cog8ToothIcon } from '@heroicons/react/24/solid'
+
 import { Dot } from "@/app/utils/TranslationUtils"
+
 
 let EachInfoCell = (props: { href?: string, className?: string, name: string, content: any }) => {
     return <a href={props.href || 'javascript:void(0);'} className={"w-full h-full   space-x-1 text-sm text-gray-400  flex flex-col text-center items-center justify-center hover:bg-zinc-50 transition-all duration-300 pt-2 pb-2 " + ' ' + props.className}>
@@ -37,15 +40,25 @@ export default () => {
             } />
         </div>
         <hr className=" mb-2"></hr>
-        <div className='flex flex-row  items-center '>
-            {
-                true ?
-                    <img src={notificationCtn != 0 ? "/controls/email.png" : "/controls/city.png"} className={"w-5 mr-2 " + (
-                        notificationCtn != 0 ? "" : " opacity-70 "
-                    )}></img> : ''
-            }
-            <RegularLink href={'/notifications'} children={<span className={COMMON_CLZ_ANCHOR_TEXT}>{Dot("2RbUh6TyJ", "{0} Unread Notifications", notificationCtn)}</span>}>
-            </RegularLink>
+        <div className='flex flex-row  justify-between items-center '>
+            <div className="flex">
+                {
+                    true ?
+                        <img src={notificationCtn != 0 ? "/controls/email.png" : "/controls/city.png"} className={"w-5 mr-2 " + (
+                            notificationCtn != 0 ? "" : " opacity-70 "
+                        )}></img> : ''
+                }
+                <RegularLink href={'/notifications'} children={<span className={COMMON_CLZ_ANCHOR_TEXT}>{Dot("2RbUh6TyJ", "{0} Unread Notifications", notificationCtn)}</span>}>
+                </RegularLink>
+            </div>
+            <div className="flex space-x-1">
+                <RegularLink href="/settings" children={(
+                    <MoonIcon className="h-5 w-5 text-blue-300" />
+                )}></RegularLink>
+                <RegularLink href="/xksd" children={(
+                    <Cog8ToothIcon className="h-5 w-5 text-zinc-400 "></Cog8ToothIcon>
+                )}></RegularLink>
+            </div>
         </div>
 
     </div>
