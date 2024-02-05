ac715339bd61ac196ee3f559f523ba991604c602
work7z <work7z@outlook.com>
2024-02-05 21:38:38 +0800

    Add dev-config.json and dev.json files, and update index.test.tsx

--- modules/elb-web/app/dao/index.tsx
+++ modules/elb-web/app/dao/index.tsx
@@ -3,14 +3,14 @@ import { SystemConfig as SystemConfig } from "../config/types"
 import fs from 'fs'
 import path from 'path'
 
-class DaoRef {
-
+type DaoRef = {
+    sequelize: Sequelize
 }
 
 export type SystemFlag = "dev" | "prod" | "test"
 
-export let getConfigByFlag = (envFlag: SystemFlag):SystemConfig => {
-    let config = fs.readFileSync(path.join(__dirname,'..','config/'+envFlag+'-config.json'),{encoding:'utf-8'})
+export let getConfigByFlag = (envFlag: SystemFlag): SystemConfig => {
+    let config = fs.readFileSync(path.join(__dirname, '..', 'config/' + envFlag + '.json'), { encoding: 'utf-8' })
     return JSON.parse(config) as SystemConfig
 }
 
@@ -27,7 +27,7 @@ export default async (envFlag: SystemFlag): Promise<DaoRef> => {
         console.error('Unable to connect to the database:', error);
     }
 
-
-
-    return {}
+    return {
+        sequelize: sequelize,
+    }
 }
