df994cf6667aa990783df6c6feb52872712552b6
work7z <work7z@outlook.com>
2024-02-09 18:20:56 +0800

    Update VerifyCodeInput component to use dynamic captcha image

--- modules/elb-web/app/api/captcha/route.tsx
+++ modules/elb-web/app/api/captcha/route.tsx
@@ -1,7 +1,25 @@
+import os from 'os'
+import path from 'path';
+import fileSystem from 'fs'
+import { readFileSync } from 'fs';
+
 export const dynamic = 'force-dynamic' // defaults to auto
-export async function GET(request: Request) {
-  let data = {
-    a: 'hello,world'
-  }
-  return Response.json({ data })
+
+
+export let getImgBase64 = (): any => {
+  let random = Math.floor(Math.random() * 10)
+  let elb3Root = process.env["ELB3_ROOT"] || ''
+  let file = path.join(elb3Root, 'precompiled', 'dev', `${random}.png`)
+  let b = readFileSync(file)
+  // return "data:image/png;base64," + b
+  return b
+}
+
+export async function GET(request: Request,response:Response) {
+  
+  return new Response(getImgBase64(),{
+    headers: {
+      "content-type": "image/png"
+    }
+  })
 }
