c1fe6de7a787457769d097325f1425a185da5299
LafTools Team - FX <work7z@outlook.com>
2024-02-12 13:00:37 +0800

    Remove unused middleware and fix string formatting in next.config.mjs

--- modules/elb-web/next.config.mjs
+++ modules/elb-web/next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-let isDev = process.env.NODE_ENV == 'development';
+let isDev = process.env.NODE_ENV == "development";
 const nextConfig = {
   // i18n: {
   //   locales: ['en-US', 'zh-CN', 'zh-TW'],
