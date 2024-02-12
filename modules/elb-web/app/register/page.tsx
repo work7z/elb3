adf02314dae84963253dbaf535fbe748023ec97a
LafTools Team - FX <work7z@outlook.com>
2024-02-12 19:35:33 +0800

    Fix loading wrapper and update registration page text

--- modules/elb-web/app/register/page.tsx
+++ modules/elb-web/app/register/page.tsx
@@ -31,7 +31,7 @@ export default function Page(props: RegisterPageProps) {
                             {Dot("o4GdNzX6xS", "Firstly, Owing to the well-known regulation requirement on the Earth, each user is required to provide a valid telephone number to register.")}
                         </div>
                         <div>
-                            {Dot("7BtpafuC", "ALso, in this community, we do not talk about politics or any other sensitve topics, and of course do not welcome spam or advertisement. That is why we require a valid telephone number for registeration, to prevent any illicit or improper activities.")}
+                            {Dot("7BtpddC", "Secondly, in this community, we do not talk about politics or any other sensitve topics, and of course do not welcome spam or advertisement. That is why we require a valid telephone number for registeration, to prevent any illicit or improper activities.")}
                         </div>
                         <div>
                             {Dot("KAiEq6dcwL5", "Lastly, as a friendly reminder, each user should take the responsibility for their own words and actions.")}
@@ -40,7 +40,16 @@ export default function Page(props: RegisterPageProps) {
                     </p>
                 </div>
             }>
-            </ CardPanel>
+            </ CardPanel>,
+            <CardPanel className="mt-2" children={
+                <div className='p-2 '>
+                    <h3 className='font-semibold mb-2'>{Dot("OPevedpOM", "What if I do not have a telephone number?")}</h3>
+                    <p className='text-xs space-y-2'>
+                        <div>{Dot("Jfr4RzEZG", "Currently, we only accept {0} telephone number to register your account.", "+86")}</div>
+                        <div>{Dot("ufSPh0ad", "If it is impossible to register your account via your current telephone number, please reach the webmaster on his email {0} or WeChat account {1}. We can assist you to register your account manually.", "work7z@outlook.com", "lafting755")}</div>
+                    </p>
+                </div>
+            }></CardPanel>
         ]
     } combindSearchProps={combindSearchProps}>
         <div className='space-y-2 flex-1'>
